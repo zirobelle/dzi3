30dfe1113dc59ee9708192185635e86c6b5e2349
zirobelle <30334708+zirobelle@users.noreply.github.com>
2017-08-16 23:02:36 +0700

    Update text.php

--- includes/text.php
+++ includes/text.php
@@ -17,7 +17,7 @@
 }
 ==============================
 */
-if (strtolower($message['text']) ++ "ดี" || $message['text'] == "ดีครับ" ){
+if (strtolower($message['text']) == "ดี" || $message['text'] == "ดีครับ" ){
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
         'messages' => array(
@@ -29,7 +29,7 @@ if (strtolower($message['text']) ++ "ดี" || $message['text'] == "ดีค�
     ));
 }
 
-else if (strtolower($message['text']) == "dzi" || $message['text'] == "ค่าสี" || $message['text'] == "เขียว"){
+if (strtolower($message['text']) == "dzi" || $message['text'] == "ค่าสี" || $message['text'] == "เขียว"){
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
         'messages' => array(
