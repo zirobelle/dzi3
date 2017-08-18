e411288eb353e23f6fac20b1f209008aada05d9e
zirobelle <30334708+zirobelle@users.noreply.github.com>
2017-08-18 08:57:08 +0700

    Update text.php

--- includes/text.php
+++ includes/text.php
@@ -23,6 +23,8 @@
 if 
 (strtolower($message['text']) ==  "Grab Color"     
 || $message['text'] == "Grab Colors"
+|| $message['text'] == "grab Colors"
+|| $message['text'] == "Grab Colors"
 || $message['text'] == "GrabColor" 
 || $message['text'] == "GrabColors" 
 
@@ -30,6 +32,10 @@ if
 || $message['text'] == "grab colors"
 || $message['text'] == "grabcolor" 
 || $message['text'] == "grabcolors"
+|| $message['text'] == "Grab color"
+|| $message['text'] == "Grab colors"
+|| $message['text'] == "Grabcolor" 
+|| $message['text'] == "Grabcolors"
 
 || $message['text'] == "grab Color"
 || $message['text'] == "grab Colors"
@@ -46,6 +52,11 @@ if
 || $message['text'] == "grab colours"
 || $message['text'] == "grabcolour" 
 || $message['text'] == "grabcolours"
+
+|| $message['text'] == "Grab colour"
+|| $message['text'] == "Grab colours"
+|| $message['text'] == "Grabcolour" 
+|| $message['text'] == "Grabcolours"
 
 || $message['text'] == "grab Colour"
 || $message['text'] == "grab Colours"
@@ -65,7 +76,12 @@ if
 || $message['text'] == "colors"
 || $message['text'] == "colour"
 || $message['text'] == "colours"
+|| $message['text'] == "Color"
+|| $message['text'] == "Colors"
+|| $message['text'] == "Colour"
+|| $message['text'] == "Colours"
 || $message['text'] == "green"
+|| $message['text'] == "Green"
 || $message['text'] == "เขียว"  
    
 )
