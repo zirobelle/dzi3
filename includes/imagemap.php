c212ac54121d82bb91a763a016a3dc14d56414ac
zirobelle <30334708+zirobelle@users.noreply.github.com>
2017-08-18 08:42:18 +0700

    Update imagemap.php

--- includes/imagemap.php
+++ includes/imagemap.php
@@ -50,7 +50,7 @@ if (strtolower($message['text']) == "Command"){
         'messages' => array(
             array(
                 'type' => 'imagemap', // 訊息類型 (圖片地圖)
-                'baseUrl' => 'https://www.gattocha.com/imgdz', // 圖片網址 (可調整大小 240px, 300px, 460px, 700px, 1040px)
+                'baseUrl' => 'https://api.reh.tw/line/bot/example/assets/images/example', // 圖片網址 (可調整大小 240px, 300px, 460px, 700px, 1040px)
                 'altText' => 'Dzi Command', // 替代文字
                 'baseSize' => array(
                     'height' => 1040, // 圖片寬
